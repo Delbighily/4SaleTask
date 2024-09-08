75ca7087bc3116e79a140ac3a593d36828fe759e
Delbighily <d.elbighily@gmail.com>
2024-09-08 14:27:40 +0300

    Creating base test configuration and some element locators and interactions

---
+++ src/main/java/pages/LandingPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LandingPage {
+    private WebDriver driver;
+    public LandingPage(WebDriver driver){this.driver=driver;}
+
+
+    //Element Locators
+    private By UserNameField= By.id("user-name");
+    private By PasswordField= By.id("password");
+    private By LoginBtn= By.id("login-button");
+
+    //Methods for interactions
+    public void fillUserName(){driver.findElement(UserNameField).sendKeys("standard_user");}
+    public void fillpassword(){driver.findElement(PasswordField).sendKeys("secret_sauce");}
+    public void clickLogin(){driver.findElement(LoginBtn).click();}
+
+
+
+}
+
---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,23 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import pages.LandingPage;
+
+import java.time.Duration;
+
+public class BaseTest {
+    private WebDriver driver;
+    protected LandingPage landingPage;
+
+    @BeforeMethod
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        landingPage =new LandingPage(driver);
+}}
