2c4546c580a0a9bb34ff3e0b742b3878e2b71e6d
Delbighily <d.elbighily@gmail.com>
2024-09-09 17:32:27 +0300

    Adding the full scenario test

---
+++ src/test/java/completingPurchaseTest/CompletePurchaseTest.java
@@ -0,0 +1,27 @@
+package completingPurchaseTest;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+import pages.CartPage;
+import pages.CompleteCheckoutPage;
+import pages.HomePage;
+import pages.InformationPage;
+
+public class CompletePurchaseTest extends BaseTest {
+    @Test
+    public void testFillingData(){
+        HomePage homePage= landingPage.clickLogin();
+        landingPage.fillUserName();
+        landingPage.fillpassword();
+        landingPage.clickLogin();
+        homePage.sortLowToHigh();
+        homePage.buyLowestPriceItem();
+        CartPage cartPage =homePage.clickCartIcon();
+        InformationPage informationPage=cartPage.ProceedCheckout();
+        informationPage.fillFirstName();
+        informationPage.fillLastName();
+        informationPage.fillPostalCode();
+        CompleteCheckoutPage completePurchaseTest= informationPage.PressContinue();
+        completePurchaseTest.pressOnFinishButton();
+
+}}
