5a1345e9a9f67bd0bc6b85af7c0ded5a9803af37
Delbighily <d.elbighily@gmail.com>
2024-09-09 15:25:04 +0300

    Adding the new pages to the configuration

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pages.CartPage;
+import pages.HomePage;
 import pages.LandingPage;
 
 import java.time.Duration;
@@ -12,6 +14,8 @@ import java.time.Duration;
 public class BaseTest {
     private WebDriver driver;
     protected LandingPage landingPage;
+    protected HomePage homePage;
+    protected CartPage cartPage;
 
     @BeforeMethod
     public void setUp(){
@@ -21,9 +25,11 @@ public class BaseTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         landingPage =new LandingPage(driver);
+        homePage=new HomePage(driver);
+        cartPage=new CartPage(driver);
 }
     @AfterMethod
     public void tearDown(){
-        driver.quit();
+        //driver.quit();
     }
 }
