0acf04e31c7c713b99d0aa0d9069118dfa1cdd7c
Delbighily <d.elbighily@gmail.com>
2024-09-09 13:02:47 +0300

    Create cart page

---
+++ src/main/java/pages/CartPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+    private WebDriver driver;
+    public CartPage(WebDriver driver){this.driver=driver;}
+
+    //Element Locators
+
+
+    //Methods for interactions
+
+
+
+}
