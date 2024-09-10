c9dbd25e2cc66cf8b256f088c4ff28817cc39fe7
Delbighily <d.elbighily@gmail.com>
2024-09-10 13:57:41 +0300

    Adding locators and methods to interact with the success message

--- src/main/java/pages/CompleteCheckoutPage.java
+++ src/main/java/pages/CompleteCheckoutPage.java
@@ -2,16 +2,21 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import utils.ConfigUtils;
 
 public class CompleteCheckoutPage {
     private WebDriver driver;
     public CompleteCheckoutPage(WebDriver driver){this.driver=driver;}
+    ConfigUtils configUtils=new ConfigUtils();
 
     //Element Locators
     private By FinishButton= By.id("finish");
+    public By SuccessMessage=By.id("checkout_complete_container");
 
     //Methods for interactions
     public void pressOnFinishButton(){
         driver.findElement(FinishButton).click();
     }
-}
+    public boolean SuccessMessageVisible(){
+        return driver.findElement(SuccessMessage).isDisplayed();
+}}
