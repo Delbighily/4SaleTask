bb2150cd5c3612c5c85590f7bd3861ce80162e85
Delbighily <d.elbighily@gmail.com>
2024-09-10 17:49:49 +0300

    Adding allure annotations

--- src/test/java/login/LoginTest.java
+++ src/test/java/login/LoginTest.java
@@ -1,12 +1,17 @@
 package login;
 
 import base.BaseTest;
-import org.openqa.selenium.WebDriver;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
 import org.testng.annotations.Test;
-import pages.LandingPage;
 
+
+@Feature("Login feature")
 public class LoginTest extends BaseTest {
-    @Test
+    @Description("In the this test the user will try to log in using valid user name and valid password")
+
+    @Test (description = "Test login with credentials provided by the website")
     public void testSuccessfullLogin(){
         landingPage.fillUserName();
         landingPage.fillpassword();
