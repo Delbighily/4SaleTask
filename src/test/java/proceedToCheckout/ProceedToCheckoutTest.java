a4e24fcf7d8c9cc7c2b3d91d19ae04d7513bcc04
Delbighily <d.elbighily@gmail.com>
2024-09-10 17:49:02 +0300

    Adding allure annotations

--- src/test/java/proceedToCheckout/ProceedToCheckoutTest.java
+++ src/test/java/proceedToCheckout/ProceedToCheckoutTest.java
@@ -1,12 +1,18 @@
 package proceedToCheckout;
 
 import base.BaseTest;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
 import org.testng.annotations.Test;
 import pages.CartPage;
 import pages.HomePage;
 
+
+@Feature("proceeding to checkout feature")
 public class ProceedToCheckoutTest extends BaseTest {
-    @Test
+    @Test (description = "Test proceeding to checkout")
+    @Description("in this test the user proceeds to his next step in the purchasing process when he goes to checkout")
+
     public void testProceedToCheckout(){
         HomePage homePage= landingPage.clickLogin();
         landingPage.fillUserName();
